26e30037ebf51eea9d377419b280b38dabaa3d64
Eric Conrad <git@zez.me>
2023-01-10 15:23:33 -0500

    Update dns-parse.py

--- dns-parse.py
+++ dns-parse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #
 # dns-parse.py
-# Extracts base32-encoded data exfiltrated to Bind query logs or Burp private Collaborator output
+# Extracts base32-encoded data exfiltrated to Bind query logs or private Burp Collaborator output
 #
 # Private Burp Collaborator output may be logged via 'tee':
 # java -jar /root/collaborator/burpsuite_pro.jar --collaborator-server | tee /root/collaborator/collaborator.log
