d78df2b7a1f7c3f5e21a65b72a0f990b42afa3ee
Eric Conrad <git@zez.me>
2023-01-10 15:21:00 -0500

    Update dns-parse.py

--- dns-parse.py
+++ dns-parse.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 #
 # dns-parse.py
+# Extracts base32-encoded data exfiltrated to Bind query logs or Burp private Collaborator logs
+#
 # Eric Conrad
 # https://ericconrad.com
 import re
@@ -14,11 +16,14 @@ if ((len(sys.argv)==4) and ((sys.argv[3]=='bind') or (sys.argv[3]=='collaborator
   with open(logname) as f:
     for line in f:
       if dnsname in line:
+        # Split the line, using all non-alphanumeric characters as delimiters. The hostname (first 
+        # label) is the 19th field in a Burp Collaborator log and the 13th field in a bind query log
         substring=re.split('\W+',line) 
         if logtype == 'collaborator':
           base32+=substring[19]
         elif logtype == 'bind':
           base32+=substring[13]
+  # base32 uses '=' signs to pad to an 8-byte boundary, restore any that are missing
   pad='=' * (8-(len(base32) % 8))
   base32+=pad
   print(base32)
