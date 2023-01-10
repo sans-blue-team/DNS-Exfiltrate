33b39e9ad7d650b0316743627b0e0f6e82ffff7a
Eric Conrad <git@zez.me>
2023-01-10 17:15:13 -0500

    Code update, more comments

--- dns-parse.py
+++ dns-parse.py
@@ -17,24 +17,25 @@
 import re
 import sys
 
-if ((len(sys.argv)==4) and ((sys.argv[3]=='bind') or (sys.argv[3]=='collaborator'))):
+if (len(sys.argv)==3):
   dnsname=sys.argv[1]
   logname=sys.argv[2]
-  logtype=sys.argv[3]
   base32=''
   with open(logname) as f:
     for line in f:
       if dnsname in line:
-        # Split the line, using all non-alphanumeric characters as delimiters. The hostname (first 
-        # label) is the 19th field in a Burp Collaborator log and the 13th field in a bind query log
+        # Split the line, using all non-alphanumeric characters as delimiters. The base32-encoded
+        # label (DNS name) is the 13th field in a bind query log and the 19th field in a Burp
+        # Collaborator log. The 7th field can be used to distingish bind ('client') from Burp
+        # Collaborator ('Received')
         substring=re.split('\W+',line) 
-        if logtype == 'collaborator':
-          base32+=substring[19]
-        elif logtype == 'bind':
+        if (substring[7] == 'client'): # Bind query log
           base32+=substring[13]
+        elif (substring[7] == 'Received'): # Burp Collaborator log
+          base32+=substring[19]
   # base32 uses '=' signs to pad to an 8-byte boundary, restore any that are missing
   pad='=' * (abs(len(base32) % -8))
   base32+=pad
   print(base32)
 else:
-  print('Usage: %s <DNS name> <log name> (bind|collaborator)' % sys.argv[0])
+  print('Usage: %s <DNS name> <log name>' % sys.argv[0])
